fa9fbdf1beb2f1c63d7ef7e0e02df629a51e55b6
Quentin <lefevre.quentin@gmail.com>
2020-05-28 14:40:17 +0200

    set map location to Liège

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -2,12 +2,11 @@ import React from "react";
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
 // import {Icon} from "leaflet";
 // import * as parkData from "./data/skateboard-parks.json";
-import style from "../../style.css";
 
 const MyMap = () => {
     return (
         
-        <Map center={[45.4, -75.7]} zoom={12}>
+        <Map center={[50.6246191, 5.5290555]} zoom={12}>,
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
