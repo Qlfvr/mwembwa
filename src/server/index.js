a3937c096cf7333ae48c84ebf1c93a31ba718ef9
Quentin <lefevre.quentin@gmail.com>
2020-06-02 09:49:25 +0200

    connect to database

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,7 @@
 
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 
 const {APP_PORT} = process.env;
 
@@ -17,7 +18,16 @@ app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.get("/hello", (req, res) => {
     console.log(`ℹ️  (${req.method.toUpperCase()}) ${req.url}`);
-    res.send("Hello, World!");
+    res.send("Hello, World!"); 
+});
+
+//Connexion à la base de donnée
+
+mongoose.connect("mongodb://mongo:27017/mwenbwa", { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("successfully connected to database");
 });
 
 app.listen(APP_PORT, () =>
