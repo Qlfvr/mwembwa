3384ba05643d2662a4788c1f27ae74c670711d4d
Martaly <aly.martin.97@gmail.com>
2020-06-05 11:45:18 +0200

    add rules

--- src/client/components/sign-in-up/sign-in-up.js
+++ src/client/components/sign-in-up/sign-in-up.js
@@ -133,7 +133,22 @@ const SignInUp = () => {
             </div>
             <div className={"rules"}>
                 <h2>{"Rules of the game"}</h2>
-                <p>{"Jeu des arbres bla bla bla"}</p>
+                <p>{"Créé ton profil et choisis une couleur."}</p>
+                <p>
+                    {
+                        "Achète des arbres pour te faire un max de feuilles! Achètes des arbres avec tes nouvelles feuilles. Tu en auras des gratuits, des payants et d'autres vérouillés par d'autres joueurs."
+                    }
+                </p>
+                <p>
+                    {
+                        "Mais attention! Toutes les 15' tu recevras une quantité de feuilles égale au total de chacun de ses arbres et toutes les heures, tu perdras la moitié de tes feuilles!"
+                    }
+                </p>
+                <p>
+                    {
+                        "N'hésites pas à consulter le classement pour voir la progression des autres joueurs... ainsi que le Gamelog qui te permettras de consulter toutes les actions du jeu."
+                    }
+                </p>
             </div>
         </div>
     );
