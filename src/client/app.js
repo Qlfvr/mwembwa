d244dc32e1aadfc0ad4ffd45a3249fce5f10d31f
Martaly <aly.martin.97@gmail.com>
2020-06-05 14:47:06 +0200

    app.js

--- src/client/app.js
+++ src/client/app.js
@@ -10,12 +10,14 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import Map from "./components/map";
 import SignInUp from "./components/sign-in-up/sign-in-up";
+import GamePage from "./components/game-page/game-page";
 import "./styles/main.scss";
 
 ReactDOM.render(
     <>
         <Map />
         <SignInUp />
+        <GamePage />
     </>,
     document.querySelector("#app"),
 );
