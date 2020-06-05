f3c6d37ea9d5ca14f824534b733550a51d88fddf
Martaly <aly.martin.97@gmail.com>
2020-06-05 10:40:19 +0200

    add modal + homepage +eslint

--- src/client/components/sign-in-up/sign-in-up.js
+++ src/client/components/sign-in-up/sign-in-up.js
@@ -1,80 +1,140 @@
-import React from "react";
+import React, {useState} from "react";
+import Modal from "react-modal";
 import {useForm} from "react-hook-form";
+
 import "./sign-in-up.scss";
 
+Modal.setAppElement("#app");
 const SignInUp = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     const {handleSubmit, register, errors} = useForm();
-    const onSubmit = (values) => console.log(values);
-
+    const onSubmit = values => console.log(values);
+    const [hex, setHex] = useState("#ffffff");
+    const randomizedHex = () => {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(
+            16,
+        )}`;
+        setHex(randomColor);
+    };
     return (
-        <div id="form-container">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h1>{"Inscription"}</h1>
-                <input
-                    name="email"
-                    ref={register({
-                        required: "Required",
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid email address",
+        <div>
+            <div className={"head"}>
+                <h1>{"Mwembwa"}</h1>
+                <button type={"button"} onClick={() => setModalIsOpen(true)}>
+                    {" Sign In / Sign Up"}
+                </button>
+
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={() => setModalIsOpen(false)}
+                    style={{
+                        overlay: {
+                            background: "grey",
                         },
-                    })}
-                />
-                {errors.email && errors.email.message}
+                    }}
+                    id={"form-container"}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h1>{"Inscription"}</h1>
+                        <label>{"Email"}</label>
+                        <input
+                            name={"email"}
+                            ref={register({
+                                required: "Required",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                    message: "invalid email address",
+                                },
+                            })}
+                        />
+                        {errors.email && errors.email.message}
+                        <label>{"Username"}</label>
+                        <input
+                            name={"username"}
+                            ref={register({
+                                validate: value =>
+                                    value !== "admin" || "Nice try!",
+                            })}
+                        />
+                        {errors.username && errors.username.message}
+                        <label>{"Password"}</label>
+                        <input
+                            name={"password"}
+                            ref={register({
+                                required: "Required",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                    message: "invalid password",
+                                },
+                            })}
+                        />
 
-                <input
-                    name="username"
-                    ref={register({
-                        validate: (value) => value !== "admin" || "Nice try!",
-                    })}
-                />
-                {errors.username && errors.username.message}
-                <input
-                    name="password"
-                    ref={register({
-                        required: "Required",
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid password",
-                        },
-                    })}
-                />
+                        {errors.password && errors.password.message}
 
-                {errors.password && errors.password.message}
+                        <label>{"Validation Password"}</label>
+                        <input
+                            name={"val-password"}
+                            ref={register({
+                                required: "Required",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                    message: "invalid password",
+                                },
+                            })}
+                        />
+                        {errors.password && errors.password.message}
 
-                <button type="submit">{"Submit"}</button>
-            </form>
+                        <button
+                            type={"button"}
+                            onClick={randomizedHex}
+                            style={{backgroundColor: `${hex}`}}>
+                            {"Pick a Color !"}
+                        </button>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h1>{"Connexion"}</h1>
-                <input
-                    name="email / username"
-                    ref={register({
-                        required: "Required",
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid email address",
-                        },
-                    })}
-                />
-                {errors.email && errors.email.message}
-                {errors.username && errors.email.massage}
+                        <button type={"submit"}>{"Go !"}</button>
+                    </form>
 
-                <input
-                    name="password"
-                    ref={register({
-                        required: "Required",
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid password",
-                        },
-                    })}
-                />
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h1>{"Connexion"}</h1>
+                        <h2>{"Happy to see you again !"}</h2>
+                        <div>
+                            <i className={"fas fa-user-alt"} />
+                        </div>
 
-                {errors.password && errors.password.message}
+                        <label>{"Email / Username"}</label>
+                        <input
+                            name={"email / username"}
+                            ref={register({
+                                required: "Required",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                    message: "invalid email address",
+                                },
+                            })}
+                        />
+                        {errors.email && errors.email.message}
+                        {errors.username && errors.email.massage}
+                        <label>{"Password"}</label>
+                        <input
+                            name={"password"}
+                            ref={register({
+                                required: "Required",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                    message: "invalid password",
+                                },
+                            })}
+                        />
 
-                <button type="submit">{"Submit"}</button>
-            </form>
+                        {errors.password && errors.password.message}
+
+                        <button type={"submit"}>{"Go !"}</button>
+                    </form>
+                </Modal>
+            </div>
+            <div className={"rules"}>
+                <h2>{"Rules of the game"}</h2>
+                <p>{"Jeu des arbres bla bla bla"}</p>
+            </div>
         </div>
     );
 };
