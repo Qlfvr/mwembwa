502e0455119071cac622ddaea41431a871f0e428
Quentin <lefevre.quentin@gmail.com>
2020-06-09 11:47:05 +0200

    display every markers for starters

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -1,7 +1,42 @@
-import React from "react";
-import {Map, TileLayer} from "react-leaflet";
+import React, {useEffect, useState} from "react";
+import {Map, TileLayer, Marker, Popup} from "react-leaflet";
+
+import axios from "axios";
 
 const MyMap = () => {
+    const [trees, setTrees] = useState({});
+
+    useEffect(() => {
+        axios
+            .get("/api/tree/")
+            .then(function (response) {
+                // handle success
+                setTrees(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }, []);
+
+    console.log(trees);
+
+    let treeMarkers = [];
+    if (Array.isArray(trees)) {
+        treeMarkers = trees.map((tree) => {
+            return (
+                <Marker position={[tree.location.lat, tree.location.lon]}>
+                    <Popup>
+                       latitude : {tree.location.lat}
+                       longitude : {tree.location.lon}
+                    </Popup>
+                </Marker>
+            );
+        });
+    }
+
+    console.log(treeMarkers);
+
     return (
         <Map center={[50.6246191, 5.5290555]} zoom={12}>
             <TileLayer
@@ -10,6 +45,7 @@ const MyMap = () => {
                     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 }
             />
+            {treeMarkers}
         </Map>
     );
 };
