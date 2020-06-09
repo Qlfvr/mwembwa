fef0f43aca02438aa49293df18a5180d81dd4391
Martaly <aly.martin.97@gmail.com>
2020-06-09 12:16:09 +0200

    add treesettings js and scss

--- src/client/components/tree-settings/tree-settings.js
+++ src/client/components/tree-settings/tree-settings.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {useForm} from "react-hook-form";
 import Modal from "react-modal";
 
 import "./tree-settings.scss";
@@ -7,6 +8,8 @@ Modal.setAppElement("#app");
 const TreeSettings = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [modalTreeIsOpen, setModalTreeIsOpen] = useState(false);
+    const {handleSubmit, register} = useForm();
+    const onSubmit = values => console.log(values);
     return (
         <div className={"tree"}>
             <button
@@ -52,43 +55,71 @@ const TreeSettings = () => {
                                     </a>
                                 </div>
                                 <div className={"buyTreeUser"}>
-                                    <i className={"fas fa-user-alt"}>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
                                         <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
                                         <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
                                         <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
                                         <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
                                         <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
                                         <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
-                                        <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
-                                        <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
-                                        <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
-                                        <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
-                                        <h3>{"User name"}</h3>
-                                    </i>
-                                    <i className={"fas fa-user-alt"}>
-                                        <h3>{"User name"}</h3>
-                                    </i>
+                                    </div>
                                 </div>
+                                <form
+                                    className={"formComment"}
+                                    onSubmit={handleSubmit(onSubmit)}>
+                                    <div className={"inputComment"}>
+                                        <input
+                                            name={"comment"}
+                                            ref={register({
+                                                pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                                },
+                                            })}
+                                        />
+                                        <i
+                                            className={"fa fa-plus-circle"}
+                                            aria-hidden={"true"}
+                                        />
+                                    </div>
+                                    <div className={"outputComment"}>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                    </div>
+                                </form>
                             </div>
                         </Modal>
                     </div>
@@ -100,6 +131,92 @@ const TreeSettings = () => {
                             onClick={() => setModalTreeIsOpen(true)}>
                             {"BUY"}
                         </button>
+                        <Modal
+                            isOpen={modalTreeIsOpen}
+                            onRequestClose={() => setModalTreeIsOpen(false)}
+                            style={{
+                                overlay: {
+                                    background: "none",
+                                },
+                            }}
+                            id={"buyTreeFree"}>
+                            <div>
+                                <div className={"buyTreeHeader"}>
+                                    <i className={"fas fa-tree"} />
+                                    <h3>{"Name of the tree"}</h3>
+                                    <h4>{"Name of the buyer"}</h4>
+                                    <a href={"src"}>
+                                        {"Link of the informations tree"}
+                                    </a>
+                                </div>
+                                <div className={"buyTreeUser"}>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                </div>
+                                <form
+                                    className={"formComment"}
+                                    onSubmit={handleSubmit(onSubmit)}>
+                                    <div className={"inputComment"}>
+                                        <input
+                                            name={"comment"}
+                                            ref={register({
+                                                pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                                },
+                                            })}
+                                        />
+                                        <i
+                                            className={"fa fa-plus-circle"}
+                                            aria-hidden={"true"}
+                                        />
+                                    </div>
+                                    <div className={"outputComment"}>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                    </div>
+                                </form>
+                            </div>
+                        </Modal>
                     </div>
                     <div className={"TreeLocked"}>
                         <i className={"fas fa-tree"} />
@@ -109,6 +226,92 @@ const TreeSettings = () => {
                             onClick={() => setModalTreeIsOpen(true)}>
                             {"BUY"}
                         </button>
+                        <Modal
+                            isOpen={modalTreeIsOpen}
+                            onRequestClose={() => setModalTreeIsOpen(false)}
+                            style={{
+                                overlay: {
+                                    background: "none",
+                                },
+                            }}
+                            id={"buyTreeFree"}>
+                            <div>
+                                <div className={"buyTreeHeader"}>
+                                    <i className={"fas fa-tree"} />
+                                    <h3>{"Name of the tree"}</h3>
+                                    <h4>{"Name of the buyer"}</h4>
+                                    <a href={"src"}>
+                                        {"Link of the informations tree"}
+                                    </a>
+                                </div>
+                                <div className={"buyTreeUser"}>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                    <div id={"buyTreeUser"}>
+                                        <i className={"fas fa-user-alt"} />
+                                        <h3>{"User name"}</h3>
+                                    </div>
+                                </div>
+                                <form
+                                    className={"formComment"}
+                                    onSubmit={handleSubmit(onSubmit)}>
+                                    <div className={"inputComment"}>
+                                        <input
+                                            name={"comment"}
+                                            ref={register({
+                                                pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                                },
+                                            })}
+                                        />
+                                        <i
+                                            className={"fa fa-plus-circle"}
+                                            aria-hidden={"true"}
+                                        />
+                                    </div>
+                                    <div className={"outputComment"}>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                        <output type={"text"}>
+                                            {"Other user comment"}
+                                            <i className={"fas fa-user-alt"} />
+                                        </output>
+                                    </div>
+                                </form>
+                            </div>
+                        </Modal>
                     </div>
                 </div>
             </Modal>
