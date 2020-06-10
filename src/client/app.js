6770ca4465df96864a5235a468e83ea81b97bbdc
Mathieu Thiry <thiry.math@gmail.com>
2020-06-10 16:09:36 +0200

    Added router react

--- src/client/app.js
+++ src/client/app.js
@@ -8,23 +8,23 @@
 
 import * as React from "react";
 import ReactDOM from "react-dom";
-import Map from "./components/map";
+import {BrowserRouter, Route} from "react-router-dom";
+import GameMap from "./components/game-map/game-map";
 import SignInUp from "./components/sign-in-up/sign-in-up";
 import GamePage from "./components/game-page/game-page";
-import TreeSettings from "./components/tree-settings/tree-settings";
+import Homepage from "./components/homepage/homepage";
 import "./styles/main.scss";
-import Leaderboard from "./components/leaderboard/leaderboard";
-import Gamelog from "./components/gamelog/gamelog";
-//import axios from "axios";
 
 ReactDOM.render(
     <>
-        <Map />
-        <SignInUp />
-        <GamePage />
-        <Leaderboard />
-        <Gamelog />
-        <TreeSettings />
+        <GameMap />
+        <BrowserRouter>
+            <div className={"main-route-place"}>
+                <Route exact path={"/"} component={Homepage} />
+                <Route exact path={"/sign-in-up"} component={SignInUp} />
+                <Route exact path={"/game-page"} component={GamePage} />
+            </div>
+        </BrowserRouter>
     </>,
     document.querySelector("#app"),
 );
