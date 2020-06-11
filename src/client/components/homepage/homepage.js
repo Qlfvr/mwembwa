0f407c8735faf0ece05e63de49313c447aadac6a
Martaly <aly.martin.97@gmail.com>
2020-06-11 16:53:36 +0200

    add

--- src/client/components/homepage/homepage.js
+++ src/client/components/homepage/homepage.js
@@ -1,71 +1,42 @@
 import React from "react";
 import Modal from "react-modal";
-import {
-    Link
-} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 import "./homepage.scss";
 
 Modal.setAppElement("#app");
-const Homepage = () => ( <
-    div className = {
-        "homepage"
-    } >
-    <
-    div className = {
-        "head"
-    } >
-    <
-    h1 > {
-        "Trouver un nom sympa"
-    } < /h1> <
-    Link to = {
-        "/sign-in-up"
-    } >
-    <
-    button className = {
-        "btn"
-    }
-    type = {
-        "button"
-    } > {
-        " Sign In / Sign Up"
-    } <
-    /button> <
-    /Link> <
-    div className = {
-        "foot"
-    } > {
-        "@ Team Estermont 2020"
-    } < /div> <
-    /div>
+const Homepage = () => (
+    <div className={"homepage"}>
+        <div className={"head"}>
+            <h1>{"Trouver un nom sympa"}</h1>
+            <Link to={"/sign-in-up"}>
+                <button className={"btn"} type={"button"}>
+                    {" Sign In / Sign Up"}
+                </button>
+            </Link>
+            <div className={"foot"}>{"@ Team Estermont 2020"} </div>
+        </div>
 
-    <
-    div className = {
-        "rules"
-    } >
-    <
-    h3 > {
-        "Rules of the game"
-    } < /h3> <
-    p > {
-        "Créé ton profil et choisis une couleur."
-    } < /p> <
-    p > {
-        "Achète des arbres pour te faire un max de feuilles! Achètes des arbres avec tes nouvelles feuilles. Tu en auras des gratuits, des payants et d'autres vérouillés par d'autres joueurs."
-    } <
-    /p> <
-    p > {
-        "Mais attention! Toutes les 15' tu recevras une quantité de feuilles égale au total de chacun de tes arbres et toutes les heures, tu perdras la moitié de tes feuilles!"
-    } <
-    /p> <
-    p > {
-        "N'hésites pas à consulter le classement pour voir la progression des autres joueurs... ainsi que le Gamelog qui te permettras de consulter toutes les actions du jeu."
-    } <
-    /p> <
-    /div> <
-    /div>
-
+        <div className={"rules"}>
+            <h3>{"Rules of the game"}</h3>
+            <p>{"Créé ton profil et choisis une couleur."}</p>
+            <p>
+                {
+                    "Achète des arbres pour te faire un max de feuilles! Achètes des arbres avec tes nouvelles feuilles. Tu en auras des gratuits, des payants et d'autres vérouillés par d'autres joueurs."
+                }
+            </p>
+            <p>
+                {
+                    "Mais attention! Toutes les 15' tu recevras une quantité de feuilles égale au total de chacun de tes arbres et toutes les heures, tu perdras la moitié de tes feuilles!"
+                }
+            </p>
+            <p>
+                {
+                    "N'hésites pas à consulter le classement pour voir la progression des autres joueurs... ainsi que le Gamelog qui te permettras de consulter toutes les actions du jeu."
+                }
+            </p>
+        </div>
+    </div>
 );
 
 export default Homepage;
--- src/client/components/sign-in-up/sign-in-up.js
+++ src/client/components/sign-in-up/sign-in-up.js
@@ -42,23 +42,6 @@ const SignInUp = () => {
             <form
                 className={"formInscription"}
                 onSubmit={handleSubmit(onSubmit)}>
-<<<<<<< HEAD
-                <h1> {"Inscription"} </h1>{" "}
-                <label className={"inputLabel"}> {"Email"} </label>{" "}
-                <input
-                    className={"inputInscription"}
-                    name={"email"}
-                    ref={register({
-                        required: "Required",
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid email address",
-                        },
-                    })}
-                />{" "}
-                {errors.email && errors.email.message}{" "}
-                <label className={"inputLabel"}> {"Username"} </label>{" "}
-=======
                 <h1>{"Inscription"}</h1>
                 <label className={"inputLabel"}>{"Email"}</label>
                 <input
@@ -76,37 +59,20 @@ const SignInUp = () => {
                 />
                 {/*errors.email && errors.email.message*/}
                 <label className={"inputLabel"}>{"Username"}</label>
->>>>>>> features/signinup
                 <input
                     className={"inputInscription"}
                     name={"username"}
                     ref={register({
                         validate: (value) => value !== "admin" || "Nice try!",
                     })}
-<<<<<<< HEAD
-                />{" "}
-                {/* {errors.username && errors.username.message} */}{" "}
-                <label className={"inputLabel"}> {"Password"} </label>{" "}
-=======
                     required
                 />
                 {/* {errors.username && errors.username.message} */}
                 <label className={"inputLabel"}>{"Password"}</label>
->>>>>>> features/signinup
                 <input
                     className={"inputInscription"}
                     name={"password"}
                     ref={register({
-<<<<<<< HEAD
-                        required: "Required",
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid password",
-                        },
-                    })}
-                />
-                {errors.password && errors.password.message}
-=======
                         // required: "Required",
                         pattern: {
                             value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/i,
@@ -116,30 +82,10 @@ const SignInUp = () => {
                     required
                 />
                 {/*errors.password && errors.password.message*/}
->>>>>>> features/signinup
                 <button
                     className={"btn"}
                     type={"button"}
                     onClick={randomizedHex}
-<<<<<<< HEAD
-                    style={{
-                        backgroundColor: `${hex}`,
-                    }}>
-                    {" "}
-                    {"Pick a Color !"}{" "}
-                </button>
-                <button className={"btn"} type={"submit"}>
-                    {" "}
-                    {"Go !"}{" "}
-                </button>{" "}
-            </form>
-            <form className={"formConnexion"} onSubmit={handleSubmit(onSubmit)}>
-                <h1> {"Connexion"} </h1>
-                <div>
-                    <i id={"icon"} className={"fas fa-user-alt avatar__icon"} />{" "}
-                </div>
-                <label className={"inputLabel"}> {"Email"} </label>{" "}
-=======
                     style={{backgroundColor: `${hex}`}}>
                     {"Pick a Color !"}
                 </button>
@@ -156,23 +102,10 @@ const SignInUp = () => {
                 </div>
 
                 <label className={"inputLabel"}>{"Email"}</label>
->>>>>>> features/signinup
                 <input
                     className={"inputConnexion"}
                     name={"email"}
                     ref={register({
-<<<<<<< HEAD
-                        required: "Required",
-                        pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                            message: "invalid email address",
-                        },
-                    })}
-                />{" "}
-                {errors.email && errors.email.message}{" "}
-                {errors.username && errors.email.massage}{" "}
-                <label className={"inputLabel"}> {"Password"} </label>{" "}
-=======
                         //required: "Required",
                         pattern: {
                             value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
@@ -184,25 +117,10 @@ const SignInUp = () => {
                 {/*errors.email && errors.email.message*/}
                 {/*errors.username && errors.email.massage*/}
                 <label className={"inputLabel"}>{"Password"}</label>
->>>>>>> features/signinup
                 <input
                     className={"inputConnexion"}
                     name={"password"}
                     ref={register({
-<<<<<<< HEAD
-                        required: "Required",
-                        pattern: {
-                            message: "invalid password",
-                        },
-                    })}
-                />
-                {errors.password && errors.password.message}
-                <button className={"btn"} type={"submit"}>
-                    {" "}
-                    {"Go !"}{" "}
-                </button>{" "}
-            </form>{" "}
-=======
                         //required: "Required",
                         pattern: {
                             value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/i,
@@ -218,7 +136,6 @@ const SignInUp = () => {
                     {"Go !"}
                 </button>
             </form>
->>>>>>> features/signinup
         </div>
     );
 };
