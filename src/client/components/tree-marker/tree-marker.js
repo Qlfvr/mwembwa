9f22c0379a0fb23ff8ed9522b0a830d7aac2ed8b
Quentin <lefevre.quentin@gmail.com>
2020-06-11 13:47:54 +0200

    add custom marker icon

--- src/client/components/tree-marker/tree-marker.js
+++ src/client/components/tree-marker/tree-marker.js
@@ -1,19 +1,18 @@
 import React, {useEffect, useState} from "react";
-import L,{ Marker, Popup} from "react-leaflet";
-
-import MarkerClusterGroup from "react-leaflet-markercluster";
+import {Marker, Popup} from "react-leaflet";
 import axios from "axios";
-
+import L from "leaflet"; // Necessary to use custom icons because not included in react-leaflet
 
 const TreeMarker = () => {
     const [trees, setTrees] = useState({});
-
     useEffect(() => {
         axios
             .get("/api/tree/")
             .then(function (response) {
                 // handle success
                 setTrees(response.data);
+                
+
             })
             .catch(function (error) {
                 // handle error
@@ -22,12 +21,36 @@ const TreeMarker = () => {
     }, []);
 
 
+
+
     let treeMarkers = [];
     if (Array.isArray(trees)) {
-        treeMarkers = trees.map((tree) => {
+        treeMarkers = trees.slice(0, 100).map((tree) => {
+
+
+    const svg =
+        `<svg xmlns='http://www.w3.org/2000/svg' width='1000' height='1000'><path d='M2,111 h300 l-242.7,176.3 92.7,-285.3 92.7,285.3 z' fill='${tree.color}'/></svg>`
+    let iconUrl = encodeURI("data:image/svg+xml," + svg).replace("#", "%23");
+
+    const icon = L.icon({
+        iconUrl: iconUrl,
+        // shadowUrl: 'leaf-shadow.png',
+        iconSize: [80, 80], // size of the icon
+        // shadowSize:   [50, 64], // size of the shadow
+        iconAnchor: [40, 40], // point of the icon which will correspond to marker's location
+        // shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor: [-35, -40], // point from which the popup should open relative to the iconAnchor
+    });
+
+
+
+
             return (
-                <Marker position={[tree.location.lat, tree.location.lon]}>
+                <Marker
+                    icon={icon}
+                    position={[tree.location.lat, tree.location.lon]}>
                     <Popup>
+                        Id : {tree._id}
                         latitude : {tree.location.lat}
                         longitude : {tree.location.lon}
                     </Popup>
@@ -38,6 +61,7 @@ const TreeMarker = () => {
 
 
     return treeMarkers;
-};
 
+    
+};
 export default TreeMarker;
