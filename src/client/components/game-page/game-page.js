432eeb5f9ea34f85a87f92f9d9ab33853e4a8389
Martaly <aly.martin.97@gmail.com>
2020-06-12 15:57:13 +0200

    Revert "add scss button gamepage"
    This reverts commit 82a25f580059fd458f604c5e6143022358261352.

--- src/client/components/game-page/game-page.js
+++ src/client/components/game-page/game-page.js
@@ -5,21 +5,21 @@ import "./game-page.scss";
 const GamePage = () => (
     <div className={"gamePage"}>
         <div className={"assets"}>
-            <button id={"buttonL"} type={"button"}>
-                <i id={"iconL"} className={"fas fa-tree"} />
+            <button id={"buttonHP"} type={"button"}>
+                <i className={"fas fa-tree"} />
 
                 <h2>{"40"}</h2>
             </button>
 
-            <button id={"buttonL"} type={"button"}>
-                <i id={"iconL"} className={"fab fa-pagelines"} />
+            <button id={"buttonHP"} type={"button"}>
+                <i className={"fab fa-pagelines"} />
 
                 <h2>{"35"}</h2>
             </button>
         </div>
         <div className={"profile"}>
             <button id={"buttonSettings"} type={"button"}>
-                <i id={"iconR"} className={"fas fa-user-cog"} />
+                <i className={"fas fa-user-cog"} />
             </button>
 
             <div id={"profilePicture"} />
@@ -29,13 +29,13 @@ const GamePage = () => (
         <div className={"actions"}>
             <Link to={"/leaderboard"}>
                 {" "}
-                <button id={"buttonF"}>
-                    <i id={"iconF"} className={"fas fa-user-shield"} />
+                <button id={"buttonHP"}>
+                    <i className={"fas fa-user-shield"} />
                 </button>
             </Link>
             <Link to={"/gamelog"}>
-                <button id={"buttonF"}>
-                    <i id={"iconF"} className={"fas fa-history"} />
+                <button id={"buttonHP"}>
+                    <i className={"fas fa-history"} />
                 </button>
             </Link>
         </div>
