560b84aedf59a9ffcf6b7899fa08e4e0ccb8c102
Mathieu Thiry <thiry.math@gmail.com>
2020-06-16 14:16:46 +0200

    Remove auth for getAllTrees()

--- src/server/routes/tree.js
+++ src/server/routes/tree.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const treeCtrl = require("../controllers/tree");
 const auth = require("../middleware/auth");
 
-router.get("/", auth, treeCtrl.getAllTrees);
+router.get("/", treeCtrl.getAllTrees);
 router.post("/set-random-trees", auth, treeCtrl.setRandomTrees);
 
 module.exports = router;
