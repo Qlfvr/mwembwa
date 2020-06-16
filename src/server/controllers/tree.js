97fb0136c1bfb5ae6a87c1e14012a2d38fd7a4d5
Mathieu Thiry <thiry.math@gmail.com>
2020-06-16 10:54:30 +0200

    Fix setRandomTrees()

--- src/server/controllers/tree.js
+++ src/server/controllers/tree.js
@@ -1,45 +1,33 @@
 const Tree = require("../models/tree");
 const User = require("../models/user");
-const tree = require("../models/tree");
 
 exports.getAllTrees = (req, res) => {
     Tree.find()
-        .then((tree) => res.status(200).json(tree))
-        .catch((error) => res.status(404).json({error}));
+        .then(tree => res.status(200).json(tree))
+        .catch(error => res.status(404).json({error}));
 };
 
 exports.setRandomTrees = (req, res) => {
-    User.findOne({email: "aaa@hotmail.com"})
-        .then((user) => {
-            if (!user) {
-                return res.status(401).json({error: "User not found"});
-            }
+    // eslint-disable-next-line consistent-return
+    User.findOne({email: "aaa@hotmail.com"}, (err, user) => {
+        if (!user) {
+            return res.status(401).json({error: "User not found"});
+        }
 
-            Tree.aggregate(
-                [{$match: {user: null}}, {$sample: {size: 3}}],
-                function (err, trees) {
-                    for (const tree of trees) {
-                        Tree.update(
-                            {_id: tree._id},
-                            {$set: {owner: user._id}},
-                            function (err) {
-                                return res
-                                    .status(201)
-                                    .json({message: "Random trees generated"});
-
-                                // .then((res) =>
-                                //     res
-                                //         .status(201)
-                                //         .json({message: "Random trees generated"}),
-                                // )
-                                // .catch((error) =>
-                                //     res.status(400).json({error: "a"}),
-                                // );
-                            },
-                        );
-                    }
-                },
-            );
-        })
-        .catch((error) => res.status(500).json({error: "b"}));
+        Tree.aggregate(
+            [{$match: {owner: null}}, {$sample: {size: 3}}],
+            // eslint-disable-next-line no-shadow
+            (err, trees) => {
+                for (const tree of trees) {
+                    Tree.updateOne({_id: tree._id}, {owner: user._id})
+                        .then(() =>
+                            res
+                                .status(200)
+                                .json({message: "Random trees generated"}),
+                        )
+                        .catch(error => res.status(404).json({error}));
+                }
+            },
+        );
+    });
 };
