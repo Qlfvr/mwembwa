2da282ca1829831f3754e2473e0a1e8d326f53fe
Mathieu <thiry.math@gmail.com>
2020-06-17 11:57:28 +0200

    Added first lines of method lockTree

--- src/server/controllers/tree.js
+++ src/server/controllers/tree.js
@@ -1,33 +1,46 @@
 const Tree = require("../models/tree");
 const User = require("../models/user");
+const helpers = require("../helpers/index");
 
 exports.getAllTrees = (req, res) => {
     Tree.find()
-        .then(tree => res.status(200).json(tree))
-        .catch(error => res.status(404).json({error}));
+        .then((tree) => res.status(200).json(tree))
+        .catch((error) => res.status(404).json({error}));
 };
 
 exports.setRandomTrees = (req, res) => {
     User.findOne({_id: req.userId})
-        .then(user => {
+        .then((user) => {
             if (!user) {
                 return res.status(401).json({error: "User not found"});
             }
 
             Tree.aggregate([{$match: {owner: null}}, {$sample: {size: 3}}])
-                .then(trees => {
+                .then((trees) => {
                     for (const tree of trees) {
                         Tree.updateOne({_id: tree._id}, {owner: user._id})
                             .then(() =>
                                 res.json({message: "Random trees generated"}),
                             )
-                            .catch(error => res.status(404).json({error}));
+                            .catch((error) => res.status(404).json({error}));
                     }
                     return true;
                 })
-                .catch(error => res.status(404).json({error}));
+                .catch((error) => res.status(404).json({error}));
             return true;
         })
-        .catch(error => res.status(404).json({error}));
+        .catch((error) => res.status(404).json({error}));
+    return true;
+};
+
+exports.lockTree = async (req, res) => {
+    try {
+        const tree = await Tree.findOne({_id: req.params.treeId});
+        const treeValue = helpers.getTreeValue(tree);
+        console.log(treeValue);
+    } catch (error) {
+        res.status(500).json({error});
+    }
+
     return true;
 };
