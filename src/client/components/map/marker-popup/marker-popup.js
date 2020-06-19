af940e33325b9633586938fb2924c7ea39f7f9ce
Quentin <lefevre.quentin@gmail.com>
2020-06-19 11:29:42 +0200

    add button for buying tree in popup

--- src/client/components/map/marker-popup/marker-popup.js
+++ src/client/components/map/marker-popup/marker-popup.js
@@ -1,12 +1,37 @@
 import React from "react";
 import {Popup} from "react-leaflet";
+import axios from "axios";
 
 const MarkerPopup = (props) => {
+    function handleClick() {
+        const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+        axios
+            .post(
+                `/api/tree/buy-one/${props.tree._id}`,
+                {},
+                {
+                    headers: {Authorization: `Bearer ${currentUser.token}`},
+                },
+            )
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }
+
     return (
         <Popup>
             Id : {props.tree._id}
             latitude : {props.tree.location.lat}
             longitude : {props.tree.location.lon}
+            <button className={"btn"} type={"submit"} onClick={handleClick}>
+                {"buy tree !"}
+            </button>
         </Popup>
     );
 };
