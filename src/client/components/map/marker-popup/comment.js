05e877d38433d182a508e35e44c82ec5886f9563
Martaly <aly.martin.97@gmail.com>
2020-06-19 15:43:50 +0200

    add js and scss comment page

--- src/client/components/map/marker-popup/comment.js
+++ src/client/components/map/marker-popup/comment.js
@@ -1,82 +1,66 @@
 import React from "react";
 import {useHistory} from "react-router-dom";
-import MarkerPopup from "./marker-popup";
 import "./comment.scss";
+import GamePage from "../../game-page/game-page";
 
 const Comment = () => {
     const history = useHistory();
 
     const routeChange = () => {
-        const path = `/market-popup`;
+        const path = `/game-page`;
         history.push(path);
     };
 
     return (
         <>
-            <MarkerPopup />
-            <div className={"formComment"} onClick={routeChange}>
+            <GamePage />
+            <div className={"container"} onClick={routeChange}>
                 <div className={"comment"}>
                     <h1>{"Comments"}</h1>
-                    <table>
-                        <thead>
-                            <input
-                                type={"text"}
-                                placeholder={"Write a comment"}
-                            />
-                            <div className={"iconComment"}>
-                                <i
-                                    id={"iconHead"}
-                                    className={"fas fa-user-alt avatar__icon"}
-                                />
-                                <i
-                                    id={"iconHead"}
-                                    className={"fas fa-plus-circle"}
-                                />
-                            </div>
-                        </thead>
-                        <tbody>
-                            <div className={"line"} />
-                            <tr>
-                                <td>
-                                    <i
-                                        id={"iconTD"}
-                                        className={
-                                            "fas fa-user-alt avatar__icon"
-                                        }
-                                    />
-                                </td>
-                                <td>
-                                    <p>{"Comment from another user"}</p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <i
-                                        id={"iconTD"}
-                                        className={
-                                            "fas fa-user-alt avatar__icon"
-                                        }
-                                    />
-                                </td>
-                                <td>
-                                    <p>{"Comment from another user"}</p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <i
-                                        id={"iconTD"}
-                                        className={
-                                            "fas fa-user-alt avatar__icon"
-                                        }
-                                    />
-                                </td>
-                                <td>
-                                    <p>{"Comment from another user"}</p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div className={"commentHead"} />
+                    <input type={"text"} placeholder={"Write a comment"} />
+                    <div className={"iconComment"}>
+                        <i
+                            id={"iconHead"}
+                            className={"fas fa-user-alt avatar__icon"}
+                        />
+                        <i id={"iconHead"} className={"fas fa-plus-circle"} />
+                    </div>
+
+                    <div className={"commentBody"} />
+                    <div className={"line"} />
+                    <div className={"commentUser"}>
+                        {" "}
+                        <i
+                            id={"iconB"}
+                            className={"fas fa-user-alt avatar__icon"}
+                        />
+                        <p>{"Comment from another user"}</p>
+                    </div>
+                    <div className={"commentUser"}>
+                        {" "}
+                        <i
+                            id={"iconB"}
+                            className={"fas fa-user-alt avatar__icon"}
+                        />
+                        <p>{"Comment from another user"}</p>
+                    </div>
+                    <div className={"commentUser"}>
+                        {" "}
+                        <i
+                            id={"iconB"}
+                            className={"fas fa-user-alt avatar__icon"}
+                        />
+                        <p>{"Comment from another user"}</p>
+                    </div>
+                    <div className={"commentUser"}>
+                        {" "}
+                        <i
+                            id={"iconB"}
+                            className={"fas fa-user-alt avatar__icon"}
+                        />
+                        <p>{"Comment from another user"}</p>
+                    </div>
                 </div>
             </div>
         </>
