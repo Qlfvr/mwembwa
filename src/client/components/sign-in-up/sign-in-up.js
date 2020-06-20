296513c3b31e5bf27d371713932d33d32194200c
Mathieu <thiry.math@gmail.com>
2020-06-20 13:56:50 +0200

    Remove Modal import

--- src/client/components/sign-in-up/sign-in-up.js
+++ src/client/components/sign-in-up/sign-in-up.js
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import Modal from "react-modal";
 import {useForm} from "react-hook-form";
 import axios from "axios";
 import {Redirect} from "react-router-dom";
