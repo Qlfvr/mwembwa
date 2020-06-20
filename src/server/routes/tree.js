f0edbadf01684849827041af7d4d49bbe189de1c
Mathieu <thiry.math@gmail.com>
2020-06-20 11:28:34 +0200

    Remove commented route

--- src/server/routes/tree.js
+++ src/server/routes/tree.js
@@ -6,8 +6,6 @@ const auth = require("../middleware/auth");
 router.get("/", treeCtrl.getAllTrees);
 router.post("/set-random-trees", auth, treeCtrl.setRandomTrees);
 router.post("/lock-tree/:treeId", auth, treeCtrl.lockTree);
-
-// router.post("/update-one/:id/", auth, treeCtrl.updateOne);
-router.post("/buy-one/:id/",   auth, treeCtrl.buyOne);
+router.post("/buy-one/:id/", auth, treeCtrl.buyOne);
 
 module.exports = router;
