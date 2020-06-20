7078e78b1b61dc18c882ed49a6a798765599157b
Mathieu <thiry.math@gmail.com>
2020-06-20 10:54:21 +0200

    Added setUserColor to signUp()

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -9,6 +9,7 @@ exports.signup = (req, res) => {
             name: req.body.name,
             email: req.body.email,
             password: hash,
+            color: req.body.color,
         });
         user.save()
             .then(() => res.status(201).json({message: "User created"}))
