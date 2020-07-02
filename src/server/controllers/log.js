b853ce2d9fc22fbed624cbd82501d3ef2e8f4ee4
Mathieu <thiry.math@gmail.com>
2020-07-02 15:00:16 +0200

    Added getAllLogs() query

--- src/server/controllers/log.js
+++ src/server/controllers/log.js
@@ -7,3 +7,53 @@ exports.add = ({action, createdBy}) => {
     });
     log.save();
 };
+
+exports.getAllLogs = async (req, res) => {
+    try {
+        const responseGetAllLogs = await Log.aggregate([
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "createdBy",
+                    foreignField: "_id",
+                    as: "createdBy",
+                },
+            },
+            {$unwind: "$createdBy"},
+            {$sort: {createdAt: -1}},
+            {
+                $project: {
+                    _id: 1,
+                    action: 1,
+                    createdAt: 1,
+                    createdBy: "$createdBy",
+                },
+            },
+        ]).exec();
+
+        const logsActionsTranslated = responseGetAllLogs.map(log => {
+            let actionTranslated = null;
+
+            switch (log.action) {
+                case "Add comment":
+                    actionTranslated = "Commentaire ajouté";
+                    break;
+                case "Tree locked":
+                    actionTranslated = "Arbre bloqué";
+                    break;
+                case "Tree purchased":
+                    actionTranslated = "Arbre acheté";
+                    break;
+                default:
+                    actionTranslated = "Arbre acheté";
+                    break;
+            }
+
+            return {...log, action: actionTranslated};
+        });
+
+        return res.status(200).json(logsActionsTranslated);
+    } catch (error) {
+        return res.status(500).json({error});
+    }
+};
