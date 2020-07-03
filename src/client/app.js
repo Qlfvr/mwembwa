d41b136adafc7702a74f507a7ad97b931e413eb3
Martaly <aly.martin.97@gmail.com>
2020-07-03 16:43:16 +0200

    retrait essai login authentification

--- src/client/app.js
+++ src/client/app.js
@@ -8,7 +8,8 @@ import Homepage from "./components/homepage/homepage";
 import Leaderboard from "./components/leaderboard/leaderboard";
 import Gamelog from "./components/gamelog/gamelog";
 import Settings from "./components/settings/settings";
-import PrivateRoute from "./private-route.js";
+//import PrivateRoute from "./private-route.js";
+//import AuthContext from "./auth.js";
 import "./styles/main.scss";
 
 ReactDOM.render(
@@ -17,10 +18,10 @@ ReactDOM.render(
         <BrowserRouter>
             <Route exact path={"/"} component={Homepage} />
             <Route exact path={"/sign-in-up"} component={SignInUp} />
-            <PrivateRoute exact path={"/game-page"} component={GamePage} />
-            <PrivateRoute exact path={"/leaderboard"} component={Leaderboard} />
-            <PrivateRoute exact path={"/gamelog"} component={Gamelog} />
-            <PrivateRoute exact path={"/settings"} component={Settings} />
+            <Route exact path={"/game-page"} component={GamePage} />
+            <Route exact path={"/leaderboard"} component={Leaderboard} />
+            <Route exact path={"/gamelog"} component={Gamelog} />
+            <Route exact path={"/settings"} component={Settings} />
         </BrowserRouter>
     </>,
     document.querySelector("#app"),
